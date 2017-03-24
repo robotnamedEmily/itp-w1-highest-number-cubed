d184d0e910aaf2a3616c4865faf1c0b3cd36b32b
Emily Cox <shmems11@gmail.com>
2017-03-24 00:46:01 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,12 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    num_list = []
+    num = 0
+    while num < limit:
+        if num ** 3 < limit:
+            num_list.append(num)
+        num += 1
+    return max(num_list)
+            
+            
